005c43bd2e7815f6711153ad848c2ac0912f5ea0
ctang415 <carrietang117@gmail.com>
2022-08-15 20:11:01 -0400

    Set up game logic

--- myscript.js
+++ myscript.js
@@ -5,13 +5,39 @@ function getComputerChoice() {
 };
 
 function playRound (playerSelection, computerSelection) {
-
+    if (playerSelection === computerSelection){
+        return "It's a tie."
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            return "You win! Rock beats scissors."
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You win! Scissors beats paper."
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock."
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "You lose! Rock beats scissors."
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You win! Paper beats rock."
+    }
+    else {
+        return "You lose! Scissors beats paper."
+    }   
     
-
-
 };
 const computerSelection = getComputerChoice().toLowerCase();
 const playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
 
 
 console.log(playRound(playerSelection, computerSelection));
+
+function game(playRound) {
+    for(let i = 0; i< 5; i++) {
+        const playerWins = 0;
+        const computerWins = 0;
+    }
+
+};
